7aaeded1066bb108e835779841b18dfd1b7f6462
henrique-furtado47 <henrique.furtadoifc@gmail.com>
2024-04-10 16:38:51 -0300

    resultado completo (talvez eu ainda mude o estilo dela)

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-function numero(string) {}
+function numero(num) {
+  var numero = document.getElementById("tela").innerHTML;
+  document.getElementById("tela").innerHTML = numero + num;
+}
+function operador(op) {
+  var numero = document.getElementById("tela").innerHTML;
+  document.getElementById("tela").innerHTML = numero + op;
+}
+function limpar() {
+  document.getElementById("tela").innerHTML = "";
+}
+function backspace() {
+  var numero = document.getElementById("tela").innerHTML;
+  document.getElementById("tela").innerHTML = numero.substring(
+    0,
+    numero.length - 1
+  );
+}
+function calcular() {
+  var resultado = document.getElementById("tela").innerHTML;
+  if (resultado) {
+    document.getElementById("tela").innerHTML = eval(resultado);
+  } else {
+    document.getElementById("tela").innerHTML = "Erro";
+  }
+}
